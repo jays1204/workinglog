e365eab045b696e7970a42c35d54376ca8e3e9a8
jays <jays.woo@sk.com>
2014-01-07 16:16:13 +0900

    e2222

--- app.js
+++ app.js
@@ -7,16 +7,26 @@ var workingDirPath = __dirname;
 var authorLog = "";
 
 //var spawnGitLog = spawn("git log --since=1.weeks --author="+ author + " --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s     %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --decorate");
-var spawnGitLog = spawn("git", ["log"], [{cwd : workingDirPath}]);
+var spawnGitLog = spawn("git", ["log", "--author="+author, "--pretty=format:'{\"message\": \"%s\", \"date\": \"%ad\"}' "], [{cwd : workingDirPath}]);
 spawnGitLog.stdout.on('data', function (data) {
   authorLog += data;
 });
 
 spawnGitLog.stderr.on('data', function (data) {
-  clog('stderr: ' + data);
+  console.log('Err: ' + data);
 });
 
 spawnGitLog.on('close', function (code) {
-  console.log(authorLog);
+//  console.log(authorLog);
+  var authorLogArr = authorLog.split(/\n/);
+
+  if (authorLogArr.length > 0) {
+    for (var i=0, li = authorLogArr.length; i < li; i++) {
+      console.log(authorLogArr[i] +"|");
+      authorLogArr[i] = JSON.parse(authorLogArr[i]);
+    }
+  }
+
+  
   console.log('exit : ' + code);
 });
